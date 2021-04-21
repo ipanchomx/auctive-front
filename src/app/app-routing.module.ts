768e319ade86ad844c164879155a7fa665fa915c
Edgar Medina <is715468@iteso.mx>
2021-04-20 20:25:04 -0500

    added auctions route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './global/guards/auth.guard';
 import { UnAuthGuard } from './global/guards/un-auth.guard';
+import { AuctionsComponent } from './pages/auctions/auctions.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
@@ -8,6 +10,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent, canActivate: [UnAuthGuard] },
   { path: 'sign-up', component: SignUpComponent, canActivate: [UnAuthGuard] },
+  { path: 'auctions', component: AuctionsComponent, canActivate: [AuthGuard] },
   // { path: '**', component: Page404Component },
   // { path: '404', component: Page404Component }
 ];
